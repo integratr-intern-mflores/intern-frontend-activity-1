826d490cc9187860a2052ec4687cf500954788bf
Melanio III Flores <mflores.intern.integratr@outlook.com>
2023-04-03 15:03:33 +0800

    Activity-2

--- script.js
+++ script.js
@@ -36,12 +36,4 @@ function formValidation(){
     }
 }
 
-    // if (password == "") {
-    //     alert("Error: The password field is Empty.");
-    // } else if (password == confirmPassword) {
-    //     alert("Logged In");
-    // } else {
-    //     alert("Please make sure your passwords match.")
-    // }
-
 
